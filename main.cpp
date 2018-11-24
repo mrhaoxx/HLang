de43d5ddf12220749be3848f4668d52d4e79f837
haoxingxing <mr.haoxx@gmail.com>
2018-11-24 15:46:24 +0800

    MAIN UPDATE

--- main.cpp
+++ main.cpp
@@ -10,10 +10,16 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	HMain->importclass("builtin", new HBuiltin);
-	HLangHelper::exec("f=new(\"function\")");
-	HLangHelper::exec("f.loadfile(\"c:\\hl\\test\")");
-	HLangHelper::exec("f.exec()");
-	//commandline c;
-	//c.show();
+	if (argc == 2)
+	{
+		HLangHelper::exec("f=new(\"function\")");
+		HLangHelper::exec("f.loadfile(\"" + QString(argv[1]) + "\")");
+		HLangHelper::exec("f.exec()");
+	}
+	else
+	{
+		commandline c;
+		c.show();
+	}
 	return app.exec();
 }
