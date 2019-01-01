b612eef06b8e14c4ac22d6f311bf02ca6e873a7a
haoxingxing <mr.haoxx@gmail.com>
2019-01-01 15:57:00 +0800

    HExecutor

--- HExecutor.cpp
+++ HExecutor.cpp
@@ -1,9 +1,22 @@
 #include "HExecutor.h"
 
-HLang::HExecutor::HExecutor()
+HPointer HLang::HExecutor::_run_(std::string __class__, std::string __function__, HArgs args)
 {
+	if (this->define->_access_(__class__) != nullptr)
+		return this->define->_access_(__class__)->__do__(__function__, args);
+	return nullptr;
 }
 
-HLang::HExecutor::~HExecutor()
+void HLang::HExecutor::_delete_(std::string __class__)
 {
+	this->define->_delete_(__class__);
+}
+
+bool HLang::HExecutor::_new_(std::string __class__, HArgs args)
+{
+	if (registeredclasses.count(__class__) != 0)
+		this->define->_import_(__class__, registeredclasses[__class__](args));
+	else
+		return false;
+	return true;
 }
--- HExecutor.h
+++ HExecutor.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "HObject.h"
+#include "HDomain.h"
 HLANG_NAMESPACE_START
+std::map < std::string, HPointer(*)(HArgs)> registeredclasses;
 class HExecutor :
 	public HObject
 {
 	HLANG_OBJECT(HExecutor);
 	HLANG_INIT_START HLANG_INIT_END
 public:
-	HPointer run(std::string __class__, std::string __function__, HArgs args);
+	HExecutor(HDomain* def = new HDomain) : define(def) {};
+	HPointer _run_(std::string __class__, std::string __function__, HArgs args);
+	bool _new_(std::string __class__, HArgs args);
+	void _delete_(std::string __class__);
+private:
+	HDomain* define;
 };
 HLANG_NAMESPACE_END
