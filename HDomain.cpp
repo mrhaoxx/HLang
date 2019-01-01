89024c0ba4709dab65aab73889a630b4838577e4
haoxingxing <mr.haoxx@gmail.com>
2019-01-01 15:42:07 +0800

    HDomain

--- HDomain.cpp
+++ HDomain.cpp
@@ -4,3 +4,48 @@ HLang::HDomain::HDomain(HDomain *upper /*= nullptr*/)
 {
 	this->upper = upper;
 }
+
+bool HLang::HDomain::_import_(std::string __name__, HPointer __ptr__)
+{
+	if (this->classes.count(__name__) != 0)
+		return false;
+	else
+		this->classes[__name__] = __ptr__;
+	return true;
+}
+
+void HLang::HDomain::_delete_(std::string __name__, bool __do_delete__ /*= true*/)
+{
+	if (this->classes.count(__name__) != 0)
+	{
+		if (__do_delete__)
+			delete classes[__name__];
+		classes.erase(__name__);
+	}
+}
+
+HPointer HLang::HDomain::_access_(std::string __name__, bool __access_upper__ /*= true*/)
+{
+	if (this->classes.count(__name__) != 0)
+		return classes[__name__];
+	else
+		if (__access_upper__)
+			if (upper != nullptr)
+				return upper->_access_(__name__);
+			else
+				return nullptr;
+		else
+			return nullptr;
+}
+
+HPointer HLang::HDomain::init_has_upper(HArgs args)
+{
+	this->upper = args[0]->to<HDomain>();
+	return HPointer(nullptr);
+}
+
+HPointer HLang::HDomain::init_has_not_upper(HArgs args)
+{
+	this->upper = nullptr;
+	return HPointer(nullptr);
+}
--- HDomain.h
+++ HDomain.h
@@ -5,9 +5,14 @@ class HDomain :
 	public HObject
 {
 	HLANG_OBJECT(HDomain);
-	HLANG_INIT_START HLANG_INIT_END
+	HLANG_INIT_START
+		HLANG_IMPORT_FUNCTION(HDomain, init, HLANG_ARGSPILT("HDomain"), &HLang::HDomain::init_has_upper);
+	HLANG_IMPORT_FUNCTION(HDomain, init, HLANG_ARGSPILT(""), &HLang::HDomain::init_has_not_upper);
+	HLANG_INIT_END
 public:
 	HDomain(HDomain *upper = nullptr);
+	HPointer init_has_upper(HArgs args);
+	HPointer init_has_not_upper(HArgs args);
 	bool _import_(std::string __name__, HPointer __ptr__);
 	void _delete_(std::string __name__, bool __do_delete__ = true);
 	HPointer _access_(std::string __name__, bool __access_upper__ = true);
