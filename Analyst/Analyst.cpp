6379b18102363d51334eef8dab2a1a30ab752505
haoxingxing <mr.haoxx@outlook.com>
2019-01-30 11:11:04 +0800

    For CMake

--- Analyst/Analyst.cpp
+++
@@ -1,8 +0,0 @@
-﻿// Analyst.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello World!\n";
-}
--- Main/HClass.cpp
+++
@@ -1,3 +0,0 @@
-#include "HClass.h"
-
-HLang::HClass::~HClass() {}
